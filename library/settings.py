7a9e4763011acee565404a0e0fb291788849a783
Danil <62389601+ignat24@users.noreply.github.com>
2021-10-16 00:06:26 +0300

    faicon fixed

--- library/settings.py
+++ library/settings.py
@@ -150,6 +150,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+# TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+# TEMPLATE_LOADERS = (
+# 'django.template.loaders.filesystem.Loader',
+# 'django.template.loaders.app_directories.Loader',)
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
